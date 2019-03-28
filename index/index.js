7d9fcd154814fd1a6c77ba1e099a334ee9827cc4
Connor Musick <cjmusick48@gmail.com>
2019-03-28 11:40:43 -0700

    playscreen progress

--- index/index.js
+++ index/index.js
@@ -3,12 +3,34 @@ window.onload = function(){
 }
 
 function playAnon(){
-    //display none to homeScreen
-
     //buildPLayScreen with Anon as name
-    buildPlayScreen("Anonomous");
+    buildPlayScreen("Anonomous", 100);
 }
 
-function buildPlayScreen(name){
+function buildPlayScreen(name, amount){
+    //hide homeScreen
+    document.getElementById("homeScreen").style.display = "none";
+
+    //create greeting header with name and add it to the playScreen
+    let welcomeDiv = document.createElement("div");
+    welcomeDiv.id = "welcomeDiv";
+    let welcomeHeader = document.createElement("h1");
+    welcomeHeader.innerHTML = "Hello " + name + ", lets play BJ!";
+
+    welcomeDiv.appendChild(welcomeHeader);
+    document.getElementById("playScreen").appendChild(welcomeDiv);
+
+    //add silhouette and style
+    let dealerDiv = document.createElement("div");
+    dealerDiv.id = "dealerDiv";
+    let dealerImg = document.createElement("img");
+    dealerImg.src = "https://lawyerbatonrouge.com/wp-content/uploads/2016/07/male-silhouette.jpg";
+    dealerImg.id = "dealerImg";
+
+    dealerDiv.appendChild(dealerImg);
+    document.getElementById("playScreen").appendChild(dealerDiv);
+
+    //add UI
+
 
 }
