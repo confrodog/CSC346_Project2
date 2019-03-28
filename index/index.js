6569d177d55ea8429fdfa3743c4c0d73433c3f3a
Andrew Enriquez <andrewenriquez@email.arizona.edu>
2019-03-28 10:41:04 -0700

    added create account

--- index/index.js
+++ index/index.js
@@ -1,7 +1,18 @@
 window.onload = function(){
     document.getElementById("anon").onclick = goToDealerScreen;
+    document.getElementById("createAccountButton").onclick = createAccount;
 }
 
 function goToDealerScreen(){
+    
+
+}
+
+function createAccount(){
+    var user = document.getElementById("username").value;
+    var pw = document.getElementById("password").value;
+    
+    console.log(user);
+    console.log(pw);
     
 }
