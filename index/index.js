7d75cb46644d0f4f434d445ea34c86e1e2145f70
Connor Musick <cjmusick48@gmail.com>
2019-03-28 12:24:38 -0700

    playscreen done

--- index/index.js
+++ index/index.js
@@ -1,3 +1,6 @@
+var user;
+var money;
+
 window.onload = function(){
     document.getElementById("anon").onclick = playAnon;
 }
@@ -8,8 +11,11 @@ function playAnon(){
 }
 
 function buildPlayScreen(name, amount){
+    user = name;
+    money = amount;
     //hide homeScreen
     document.getElementById("homeScreen").style.display = "none";
+    document.getElementById("playScreen").style.display = "block";
 
     //create greeting header with name and add it to the playScreen
     let welcomeDiv = document.createElement("div");
@@ -30,7 +36,49 @@ function buildPlayScreen(name, amount){
     dealerDiv.appendChild(dealerImg);
     document.getElementById("playScreen").appendChild(dealerDiv);
 
+    //THIS IS WHERE THE UI WILL CHANGE WITH SCREENS( WANT TO KEEP DEALER AND NAME THERE )
+    
+    let subPlayScreen1 = document.createElement("div");
+    subPlayScreen1.id = "subPlayScreen1";
+
+    //add amount of money user has
+    let moneyDiv = document.createElement("div");
+    moneyDiv.id = "moneyDiv";
+    let moneyH2 = document.createElement("h2");
+    moneyH2.innerHTML = "You have $" + amount + " to bet with."
+    moneyDiv.appendChild(moneyH2);
+    subPlayScreen1.appendChild(moneyDiv);
+
     //add UI
+    let UIDiv = document.createElement("div");
+    UIDiv.id = "UIDiv";
+    let playButton = document.createElement("button");
+    playButton.innerHTML = "PLAY";
+    playButton.id = "playButton";
+    playButton.onclick = playButtonPressed;
+    let leaveButton = document.createElement("button");
+    leaveButton.innerHTML = "LEAVE";
+    leaveButton.id = "leaveButton";
+    leaveButton.onclick = leaveButtonPressed;
 
+    UIDiv.appendChild(playButton);
+    UIDiv.appendChild(leaveButton);
+    subPlayScreen1.appendChild(UIDiv);
+    document.getElementById("playScreen").appendChild(subPlayScreen1);
+}
 
+function playButtonPressed(){
+    //clear sub screen and update welcome
+    document.getElementById("subPlayScreen1").innerHTML = "";
+    document.getElementById("welcomeHeader").innerHTML = "Enter a bet," + user;
+    //create new UI
+    let subPlayScreen2 = document.createElement("div");
+    let betScreen = document.createElement("div");
+    betScreen.id = "betScreen";
+    
+}
+//make the playscreen go away and make the home screen come back
+function leaveButtonPressed(){
+    document.getElementById("playScreen").innerHTML = "";
+    document.getElementById("homeScreen").style.display = "block";
 }
