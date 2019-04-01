9e2806062c1b3b1970c342f6c42f08c954a89ec5
Connor Musick <cjmusick48@gmail.com>
2019-03-31 21:03:50 -0700

    moving files

--- index_service.js
+++ index_service.js
@@ -1,12 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 var express = require('express');
 
 var app = express();
+app.configure(function(){
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.bodyParser());
+    app.use(express.logger("short"));
+});
 var port = 9000;
 
 app.get('/', function(req, res){
-    res.send('homepage');
+    res.sendFile(filedir + '/index.html');
 });
 
 app.listen(port);
