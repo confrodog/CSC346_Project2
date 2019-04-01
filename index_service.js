a41a37f15f3c731b4264f7e2ada5f6eb51123e21
Connor Musick <cjmusick48@gmail.com>
2019-03-31 21:08:05 -0700

    oops

--- index_service.js
+++ index_service.js
@@ -4,15 +4,15 @@ var path = require('path');
 var express = require('express');
 
 var app = express();
-app.configure(function(){
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.bodyParser());
-    app.use(express.logger("short"));
-});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.bodyParser());
+app.use(express.logger("short"));
+
 var port = 9000;
 
 app.get('/', function(req, res){
-    res.sendFile(filedir + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(port);
