24f9d6a873965200a74875c212cc8085c9f5972e
Connor Musick <cjmusick48@gmail.com>
2019-04-03 07:48:59 -0700

    require mysql

--- index_service.js
+++ index_service.js
@@ -1,22 +1,8 @@
 //var http = require('http');
 (function () {
     "use strict";
-var fs = require('fs');
-/*
-MAKES IT APPEAR ON HTTP
-var path = require('path');
-var express = require('express');
-var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-var port = 9000;
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-app.listen(port);
-console.log("Now listening on port: " + port);
-*/
-
 const express = require("express");
+var mysql = require('mysql');
 const app = express();
 app.use(express.static('public'));
 
